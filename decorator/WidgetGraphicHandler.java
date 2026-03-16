94c2669e3d69ff67ac642bf58604ead04ee9c3bc
HalfOfTwoBiscuits <40010399@petroc.ac.uk>
2026-03-16 16:10:09 +0000

    fix mistyped variable name in comment

--- decorator/WidgetGraphicHandler.java
+++ decorator/WidgetGraphicHandler.java
@@ -3,7 +3,7 @@ public interface WidgetGraphicHandler {
     // constructs and returns some graphics for a widget.
     // It is implemented by both the main widget class and its decorators,
     // and used as the data type for the important 
-    // `AbstractWidgetDecorator.nextToRender` variable.
+    // `AbstractWidgetDecorator.decoratedWidget` variable.
 
     public String render();
 }
