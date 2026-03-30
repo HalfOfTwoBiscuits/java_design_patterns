ac8a58df6b5c7786a68db8e910d6c7f6d0d01469
HalfOfTwoBiscuits <jaki@smileyfacebooks.co.uk>
2026-03-30 19:00:12 +0100

    improve logging

--- src/observer/Product.java
+++ src/observer/Product.java
@@ -1,18 +1,20 @@
 package observer;
 
 class Product extends AbstractSubject {
-    private String name;
+    private final String name;
     private int amountInStock;
 
     public Product(String productName, int stock) {
         name = productName;
         amountInStock = stock;
+        logAmountInStock();
     }
 
     public void buy(int amount) throws UnsupportedOperationException {
-        System.out.println("Buying " + String.valueOf(amount) + " of " + name + ". " + String.valueOf(amountInStock) + " in stock.");
+        System.out.println(String.valueOf(amount) + " " + name + "(s) were sold.");
         if (isInStock(amount)) {
             amountInStock = amountInStock - amount;
+            logAmountInStock();
         }
         else {
             throw new UnsupportedOperationException("Attempt to purchase more of a product than is in stock.");
@@ -21,15 +23,22 @@ class Product extends AbstractSubject {
     public void buy() {buy(1);}
 
     public void restock(int amount) {
-        System.out.println("Restocking " + String.valueOf(amount) + " of " + name + ". " + String.valueOf(amountInStock) + " in stock.");
+        System.out.println("Restocking +" + String.valueOf(amount) + " " + name + "(s).");
+
         boolean wasInStock = isInStock();
         amountInStock = amountInStock + amount;
+        logAmountInStock();
+
         if (!wasInStock) {
-            System.out.println("Sending notification...");
+            System.out.println("Back in stock again! Sending notification...");
             notify_();
         }
     }
 
+    private void logAmountInStock() {
+        System.out.println("There are " + String.valueOf(amountInStock) + " " + name + "(s) in stock.");
+    }
+ 
     public String getName() {return name;}
     public boolean isInStock() {return amountInStock > 0;}
     public boolean isInStock(int minimum) {return amountInStock >= minimum;}
